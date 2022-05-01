1889d5aa355b6183311f4ab7a576da60eb3d440c
tianshu <thetianshuhuang@gmail.com>
2022-05-01 17:46:59 -0400

    Added jax plots

--- dataset.py
+++ dataset.py
@@ -1,12 +1,20 @@
 """Dataset for prediction."""
 
-import numpy as np
-import torch
+from functools import partial
+from collections import namedtuple
 
+import numpy as np
+from jax import numpy as jnp
+
+from jax import random, vmap
+from sklearn.decomposition import PCA
+
+
+IndexSplit = namedtuple("IndexSplit", ['train', 'val'])
 
 class Dataset:
     """Dataset.
-    
+
     Parameters
     ----------
     data : str or dict
@@ -20,52 +28,100 @@ class Dataset:
     """
 
     def __init__(
-            self, data="data/polybench/20-40.npz", device=None,
-            val=0.5, test=0.0):
+            self, data="data/polybench/20-40.npz", device=None):
 
         if isinstance(data, str):
             data = dict(np.load(data))
         self.data = data
-        self.matrix = np.log(data['runtime'])
-        self.rms = np.sqrt(np.mean(np.square(self.matrix)))
-        self.matrix_torch = torch.tensor(self.matrix)
+        self.matrix = jnp.log(data['runtime'])
+        self.rms = jnp.sqrt(jnp.mean(jnp.square(self.matrix)))
         if device is not None:
             self.matrix_torch = self.matrix_torch.to(device)
 
-        self.opcodes = np.log(data['opcodes'].astype(np.float32) + 1)
+        opcodes_nonzero = np.sum(data['opcodes'], axis=0) > 0
+        _opcodes = data['opcodes'][:, opcodes_nonzero]
+        self.opcodes = jnp.log(_opcodes.astype(jnp.float32) + 1)
+        self.opcodes_pca = jnp.array(PCA().fit_transform(self.opcodes))
+        self.opcode_names = np.where(opcodes_nonzero)
+
         self.files = data['files']
         self.runtimes = data['runtimes']
-        self.size = np.sum(data['runtime'] > 0)
+        self.size = jnp.sum(data['runtime'] > 0)
 
-        self._split(val, test)
+    def grid(self):
+        """Create (x, y) grid pairs."""
+        x, y = jnp.meshgrid(
+            jnp.arange(self.matrix.shape[0]), jnp.arange(self.matrix.shape[1]))
+        return jnp.stack([x.reshape(-1), y.reshape(-1)]).T
 
-    def _split(self, val=0.5, test=0.):
+    def split_iid(self, key, p=0.5):
+        """Create Data split."""
 
-        x, y = np.meshgrid(
-            np.arange(self.matrix.shape[0]), np.arange(self.matrix.shape[1]))
-        xy = np.stack([x.reshape(-1), y.reshape(-1)]).T
-        idx = np.arange(xy.shape[0])
-        np.random.shuffle(idx)
+        xy = self.grid()
+        idx = random.permutation(key, jnp.arange(xy.shape[0]))
 
-        val_size = int(val * self.size)
-        test_size = int(test * self.size)
-        train_size = self.size - val_size - test_size
+        train_size = int(xy.shape[0] * p)
+        return IndexSplit(train=xy[idx[:train_size]], val=xy[idx[train_size:]])
 
-        self.splits = {
-            'train': xy[idx[:train_size]],
-            'val': xy[idx[train_size: train_size + val_size]],
-            'test': xy[idx[train_size + val_size:]]
-        }
+    def split_kfold(self, key, k=2):
+        """Perform k-fold data split into train and validation sets.
+        
+        Each assignment is IID, so there may be some variance -- n(k-1) / k^2
+        in the exact split numbers.
 
-    def loss(self, pred, idx=None, split='val'):
-        """Compute loss."""
-        if idx is None:
-            idx = self.splits[split]
-        y_true = self.matrix_torch[idx[:, 0], idx[:, 1]]
-        return torch.mean(torch.square(pred - y_true))
+        Parameters
+        ----------
+        key : jax.random.PRNGKey
+            Root PRNGKey for this operation.
+        k : int
+            Number of splits.
+        """
+        xy = self.grid()
+        idx = random.permutation(key, jnp.arange(xy.shape[0]))
+        size = int(xy.shape[0] / k)
 
-    def sample(self, batch=64, split='train'):
-        """Get split of indices."""
-        split = self.splits[split]
-        choices = np.random.choice(np.arange(split.shape[0]), size=batch)
-        return split[choices]
+        orders = xy[idx[(
+            jnp.arange(xy.shape[0]).reshape(1, -1)
+            + jnp.arange(k).reshape(-1, 1) * size
+        ) % xy.shape[0]]]
+
+        return IndexSplit(train=orders[:, :size, :], val=orders[:, size:, :])
+
+    def split(self, key, splits=100, p=0.25):
+        """Generate data splits using n sets of k-fold splits or IID splits.
+
+        Parameters
+        ----------
+        key : jax.random.PRNGKey
+            Root PRNGKey for this operation.
+        splits : int
+            Number of splits to generate.
+        p : int
+            Split proportion. If approximately 1/k, uses k-fold; otherwise,
+            uses IID splits.
+        """
+        if p < 0.5 and np.abs(int(1 / p) - 1 / p) < 0.05:
+            k = int(1 / p)
+            n = int(splits / k)
+            _, *keys = random.split(key, n + 1)
+            splits = vmap(partial(self.split_kfold, k=k))(jnp.array(keys))
+            return IndexSplit(
+                train=splits.train.reshape(-1, *splits.train.shape[2:]),
+                val=splits.val.reshape(-1, *splits.val.shape[2:]))
+        else:
+            _, *keys = random.split(key, splits + 1)
+            splits = vmap(partial(self.split_iid, p=p))(jnp.array(keys))
+            return splits
+
+    def to_mask(self, xy):
+        """Convert indices to mask."""
+        return jnp.zeros_like(self.matrix).at[xy[:, 0], xy[:, 1]].set(1.)
+
+    def index(self, indices):
+        """Index into data."""
+        return self.matrix[indices[:, 0], indices[:, 1]]
+
+    def loss(self, pred, indices):
+        """Compute l2-log loss."""
+        y_true = self.index(indices)
+        return jnp.mean(jnp.square(pred - y_true))
