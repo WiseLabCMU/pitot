a8eaa7647ba575e040b4bd9abaa9fa10f4c29139
Tianshu <tianshu2@andrew.cmu.edu>
2022-08-28 21:22:59 -0400

    Allow selecting dataset in experiments.py

--- experiments.py
+++ experiments.py
@@ -13,59 +13,61 @@ from prediction import CrossValidationTrainer
 from prediction.models import *
 
 
-ds = Dataset("data.npz", offset=1000. * 1000.)
 Method = namedtuple(
     "Method", ["constr", "kwargs", "optimizer", "epochs", "epoch_size"])
 
 COMMON = {"epochs": 250, "epoch_size": 100, "optimizer": optax.adam(0.001)}
 KWARGS_COMMON = {"layers": [64, 32], "dim": 4, "scale": 0.01}
-
-MODELS = {
-    "embedding": Method(
-        constr=embedding,
-        kwargs={
-            "runtime_data": ds.runtime_data, "module_data": ds.module_data,
-            **KWARGS_COMMON},
-        **COMMON),
-    "runtime_only": Method(
-        constr=embedding,
-        kwargs={"runtime_data": ds.runtime_data, **KWARGS_COMMON},
-        **COMMON),
-    "module_only": Method(
-        constr=embedding,
-        kwargs={"module_data": ds.module_data, **KWARGS_COMMON},
-        **COMMON),
-    "linear4": Method(
-        constr=linear,
-        kwargs={"dim": 4, "scale": 0.01},
-        **COMMON),
-    "linear8": Method(
-        constr=linear,
-        kwargs={"dim": 8, "scale": 0.01},
-        **COMMON),
-    "linear16": Method(
-        constr=linear,
-        kwargs={"dim": 16, "scale": 0.01},
-        **COMMON),
-    "linear32": Method(
-        constr=linear,
-        kwargs={"dim": 32, "scale": 0.01},
-        **COMMON),
-    "linear64": Method(
-        constr=linear,
-        kwargs={"dim": 64, "scale": 0.01},
-        **COMMON),
-    "simple_mlp": Method(
-        constr=MLPOnly,
-        kwargs={
-            "runtime_data": ds.runtime_data, "module_data": ds.module_data},
-        **COMMON),
-}
-
 SPARSITY = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 
-def _experiment(name, method, p, replicates=100, baseline=True):
+def _models(name, dataset):
+    return {
+        "embedding": Method(
+            constr=embedding,
+            kwargs={
+                "runtime_data": dataset.runtime_data,
+                "module_data": dataset.module_data,
+                **KWARGS_COMMON},
+            **COMMON),
+        "runtime_only": Method(
+            constr=embedding,
+            kwargs={"runtime_data": dataset.runtime_data, **KWARGS_COMMON},
+            **COMMON),
+        "module_only": Method(
+            constr=embedding,
+            kwargs={"module_data": dataset.module_data, **KWARGS_COMMON},
+            **COMMON),
+        "linear4": Method(
+            constr=linear,
+            kwargs={"dim": 4, "scale": 0.01},
+            **COMMON),
+        "linear8": Method(
+            constr=linear,
+            kwargs={"dim": 8, "scale": 0.01},
+            **COMMON),
+        "linear16": Method(
+            constr=linear,
+            kwargs={"dim": 16, "scale": 0.01},
+            **COMMON),
+        "linear32": Method(
+            constr=linear,
+            kwargs={"dim": 32, "scale": 0.01},
+            **COMMON),
+        "linear64": Method(
+            constr=linear,
+            kwargs={"dim": 64, "scale": 0.01},
+            **COMMON),
+        "simple_mlp": Method(
+            constr=MLPOnly,
+            kwargs={
+                "runtime_data": dataset.runtime_data,
+                "module_data": ds.module_data},
+            **COMMON),
+    }[name]
+
+
+def _experiment(name, method, p, replicates=100, baseline=True, dataset=None):
     trainer = CrossValidationTrainer(
         ds, partial(method.constr, shape=ds.shape, **method.kwargs),
         optimizer=method.optimizer, replicates=replicates, k=25,
@@ -84,14 +86,16 @@ if __name__ == "__main__":
 
     p = ArgumentParser()
     p.add_argument("methods", nargs="+", default=[])
-    p.add_argument("--replicates", type=int, default=100)
+    p.add_argument("--replicates", "-n", type=int, default=100)
+    p.add_argument("--dataset", "-d", default="data_cfs.npz")
     p.add_argument("--no-baseline", dest="baseline", action='store_false')
     p.set_defaults(baseline=True)
     args = p.parse_args()
 
+    ds = Dataset(args.dataset, offset=1000. * 1000.)
     for name in args.methods:
         model_dir = os.path.join("results", name)
         for s in SPARSITY:
             _experiment(
-                name, MODELS[name], s, replicates=args.replicates,
-                baseline=args.baseline)
+                name, _models(name, ds), s, replicates=args.replicates,
+                baseline=args.baseline, dataset=ds)
